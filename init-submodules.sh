11b16c9e9b751d1ec216e68591316f44a9869cf3
Thomas Haller <thomashaller@gmx.at>
2024-05-23 16:09:04 +0200

    fixed submodule init script and honey-badger-testing install

--- init-submodules.sh
+++ init-submodules.sh
@@ -2,12 +2,12 @@
 
 echo "getting defined submodules"
 # git submodule update -r
-git submodule init -r
-
+git submodule update --init --recursive
 
 echo "warning: NPM Version requirements are not checked in this script"
 echo "local instal of node dependencies"
 cd diamond-contracts-core && npm i && cd ..
 cd diamond-contracts-dao && npm i && cd ..
 cd diamond-contracts-claiming && npm i && cd ..
-cd honey-badger-testing && nvm use && npm i && cd ..
+# note: honey-badger-testing has postinstall scripts that use the other projects.
+cd honey-badger-testing && npm i && cd ..
