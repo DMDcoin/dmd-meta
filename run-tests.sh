047a8f75fb9e6615dc02dc50cc2535aebe26a62c
Thomas Haller <thomashaller@gmx.at>
2024-05-26 19:36:25 +0200

    loggin used version for tests

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,9 @@ mkdir -p out/latest
 starttime=$(date +"%Y_%m_%d_%H_%M_%S")
 
 echo $starttime >> out/latest/starttime.txt
+
+git submodule -q foreach 'git remote get-url origin --push && git rev-parse HEAD' > out/latest/versions.txt 
+
 cd honey-badger-testing && npm run testnet-testrun-auto-restake >> out/latest/main.log || true
 
 # cleanup
