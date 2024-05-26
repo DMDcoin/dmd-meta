e04da19f3620bdb937633b514db32fea62a0a96e
Thomas Haller <thomashaller@gmx.at>
2024-05-26 19:09:16 +0200

    store logs of output.

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 echo "Executes End2End test on a initialized setup."
+rm -rf /some/dir
+mkdir -p out/latest
 
-cd honey-badger-testing && npm run testnet-testrun-auto-restake
+starttime=$(date +"%Y_%m_%d_%H_%M_%S")
+
+echo $starttime >> out/latest/starttime.txt
+cd honey-badger-testing && npm run testnet-testrun-auto-restake >> out/latest/main.log || true
+
+# cleanup
+pkill diamond-node
