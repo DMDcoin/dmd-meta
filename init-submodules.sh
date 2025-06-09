b52dd7d282f99c7f0f76709973942180873fe1fc
Thomas Haller <thomashaller@gmx.at>
2025-06-09 23:57:53 +0200

    specifying rust 1.85 for install

--- init-submodules.sh
+++ init-submodules.sh
@@ -13,4 +13,5 @@ cd diamond-contracts-views && npm i && cd ..
 
 cd diamond-dev-tools && npm i && npm run localnet-create-mnemonic && cd ..
 
+rustup default 1.85
 cd diamond-node && export RUSTFLAGS='-C target-cpu=native' && cargo build --release
